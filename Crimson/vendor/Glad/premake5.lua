cade505bf8f7307ce73883a43ff3809e7649119d
Ryan Mangeno <ryanmangeno@gmail.com>
2024-12-26 04:58:38 -0500

    premake for static lib

--- Crimson/vendor/Glad/premake5.lua
+++ Crimson/vendor/Glad/premake5.lua
@@ -1,5 +1,6 @@
 project "Glad"
     kind "StaticLib"
+    staticruntime "on"
     language "C"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
@@ -20,9 +21,12 @@ project "Glad"
     
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
 
 
-    
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
